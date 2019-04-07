1171d401437f3334e39b975c978421e3dfd9f3d9
mcarpenter-ucvts <43757511+mcarpenter-ucvts@users.noreply.github.com>
2019-04-07 13:59:36 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 var i = 0;
-var machineStatus = 0;
+var minerStatus = 0;
 var stopGame;
-var numMachines = 0;
-var factStatus = 0;
-var numFact = 0;
+var numMiner = 0;
+var drillStatus = 0;
+var numDrill = 0;
 
 function countInsert(){
   document.count.display.value++
@@ -17,10 +17,10 @@ function reset(){
   document.count.display.value = 0;
   i=0;
   stopGame = true;
-  machineStatus = 0;
-  numMachines = 0;
-  numFact = 0;
-  factStatus = 0;
+  minerStatus = 0;
+  numMiner = 0;
+  numDrill = 0;
+  drillStatus = 0;
   document.numAutoMiners.numAutoMiners.value = 0;
   start();
 }
@@ -29,23 +29,23 @@ function buyMachine(){
   if(document.count.display.value>=50){
     document.count.display.value = document.count.display.value - 50;
     console.log(i);
-    numMachines++
+    numMiner++
     machineStatus=numMachines;
     numAutoMiners();
-    machineStat();
+    minerStat();
   }else {
-    alert("Insufficient Funds");
+    alert("Need more Diamonds!!");
   }
 }
 
 setInterval(function(){
-  if(machineStatus >= 1 && stopGame == false){
+  if(minerStatus >= 1 && stopGame == false){
     let stored = Number(document.count.display.value)
     document.count.display.value = "";
     console.log(typeof stored);
-    document.count.display.value = (stored + (machineStatus * 1));
+    document.count.display.value = (stored + (minerStatus * 1));
   }
-  machineStat();
+  minerStat();
 }, 1000)
 
 setInterval(function(){
@@ -56,10 +56,10 @@ setInterval(function(){
 }, 5000)
 
 function machineStat(){
-  console.log(machineStatus)
-  if(machineStatus >= 1){
-    let numOfMachines = machineStatus
-    return machineStatus
+  console.log(minerStatus)
+  if(minerStatus >= 1){
+    let numOfMachines = minerStatus
+    return minerStatus
   }
 }
 function start(){
@@ -79,16 +79,16 @@ function numAutoDrills(){
 function buyDiamondDrill(){
   if(document.count.display.value >= 1000){
     document.count.display.value = document.count.display.value - 1000;
-    numFact++
-    console.log(numFact);
+    numDrill++
+    console.log(numDrill);
     numAutoDrills();
   }else{
-    alert("Invalid Funds!");
+    alert("Need more Diamonds!!");
   }
 }
 setInterval(function(){
-  if(numFact >= 1 && stopGame == false){
-    machineStatus = machineStatus + numFact;
-    document.numAutoMiners.numAutoMiners.value = machineStatus;
+  if(numDrill >= 1 && stopGame == false){
+    minerStatus = minerStatus + numDrill;
+    document.numAutoMiners.numAutoMiners.value = minerStatus;
   }
 }, 2021)
